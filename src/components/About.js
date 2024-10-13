80367b0205a67b9ad4358b24d6ccb4d64e350879
Nadeem Sheriff A <137400851+nadeemsheriff18@users.noreply.github.com>
2024-10-13 18:35:22 +0530

    third attempt

--- src/components/About.js
+++ src/components/About.js
@@ -3,32 +3,38 @@ import { motion } from "framer-motion";
 
 const About = () => {
   return (
-    <div name='about' id="about" className='w-full h-auto pt-40 sm:pt-0 md:pt-0 lg:pt-0 text-gray-300 bg-gray-900'>
+    <div name='about' id="about" className='w-full h-auto pt-56 sm:pt-0 md:pt-0 lg:pt-0 text-gray-300 bg-gray-900'>
       <div className='flex flex-col justify-center items-center w-full h-full px-4 sm:px-6 md:px-10 lg:px-16 xl:px-32 py-20'>
-        <div className='px-20'>
-        <motion.div
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 0.5 }}
-          
-         className='mb-8'>
+        <div className='px-4 sm:px-20'>
+          <motion.div
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 0.5 }}
+            className='mb-8'
+          >
             <p id="about_header" className='motion-safe:animate-bounce text-4xl font-mono sm:text-6xl mt-5 lg:text-7xl font-extrabold'>
               About Me
             </p>
           </motion.div>
           <div>
-            <motion.h2 whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 1 }} className='text-sm font-bold sm:text-xl lg:text-2xl pb-3 lg:py-4 w-full font-mono'>
+            <motion.h2
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className='text-base font-bold sm:text-xl lg:text-2xl pb-3 lg:py-4 w-full font-mono text-center sm:text-left'
+            >
               Heyy, I'm Nadeem Sheriff, a web developer with a strong foundation in full-stack development.
               <br />
-              <span className='text-2xl text-justify sm:text-3xl lg:text-3xl font-extrabold block'>
+              <span className='text-xl text-justify sm:text-2xl lg:text-3xl font-extrabold block'>
                 Nice to meet you!
               </span>
             </motion.h2>
-            <motion.p whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 8 }} className='text-base font-semibold font-mono py-2 w-full sm:text-xl md:text-2xl lg:text-2xl px-0'>
+            <motion.p
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className='text-base font-semibold font-mono py-2 w-full sm:text-lg md:text-xl lg:text-2xl px-0 text-justify'
+            >
               I specialize in building dynamic web applications, leveraging technologies like React, Node.js, Express, and structured databases such as SQL and PostgreSQL. My experience extends into designing RESTful APIs, ensuring efficient and scalable solutions.
               <br />
               I’m passionate about technology, constantly exploring new trends, and I have a keen interest in networks, cybersecurity, and machine learning. This combination of skills allows me to create secure, robust, and innovative applications, and I’m always eager to take on new challenges in the tech space.
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import {motion} from "framer-motion";
+import React from 'react';
+import { motion } from "framer-motion";
+
 const Projects = () => {
   return (
     <div id="projects" className='w-full h-max'>
@@ -7,9 +8,10 @@ const Projects = () => {
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
-        className='py-20 text-center text-5xl md:text-6xl lg:text-7xl text-white' id='projects_header'>My projects</motion.h1>
-      <div className='flex flex-col px-28 py-2 font-mono text-lg'>
+        className='py-20 text-center text-5xl md:text-6xl lg:text-7xl text-white' id='projects_header'>My Projects</motion.h1>
 
+      <div className='flex flex-col px-4 sm:px-10 lg:px-28 py-2 font-mono text-lg'>
+        {/* Project 1 */}
         <div className="flex flex-wrap lg:justify-center py-8 my-1 text-white">
           <motion.div
             whileInView={{ opacity: 1, x: 0 }}
@@ -17,82 +19,80 @@ const Projects = () => {
             transition={{ duration: 1 }}
             className='w-full lg:w-1/4'>
             <img src='/assets/od_app.png' alt="OD request app" width={250} height={250} className='mb-6 rounded' />
-
-
           </motion.div>
           <motion.div 
-           whileInView={{opacity:1,x:0}}
-            initial={{opacity:0,x:100}}
-           transition={{duration:1}}
-          className='w-full max-w-xl flex flex-col lg:w-3/4'>
-            <h6 className='mb-2 font-extrabold'>OD/Leave request application</h6>
-            <p className='mb-2 text-neutral-400'>This project was developed for my college, A On-duty(OD) and leave management system for students and HOD streamlining the Request and approval process with a centralized dashboard, real-time updates, and document uploads. Simplifes the process of applying on-duty and leave for students .  </p>
-            <p className='text-sm'>React js,Express js,Postgresql,Tailwindcss</p>
-            <a  className="text-lg px-2 py-2 hover:animate-bounce font-extrabold" href='https://github.com/nadeemsheriff18/od_project'>Github</a>
+           whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: 100 }}
+           transition={{ duration: 1 }}
+           className='w-full max-w-xl flex flex-col lg:w-3/4'>
+            <h6 className='mb-2 font-extrabold'>OD/Leave Request Application</h6>
+            <p className='mb-2 text-neutral-400 text-justify'>This project was developed for my college, a On-duty (OD) and leave management system for students and HOD streamlining the request and approval process with a centralized dashboard, real-time updates, and document uploads. Simplifies the process of applying for on-duty and leave for students.</p>
+            <p className='text-sm'>Technologies: React, Express, PostgreSQL, Tailwind CSS</p>
+            <a className="text-lg px-2 py-2 hover:animate-bounce font-extrabold" href='https://github.com/nadeemsheriff18/od_project'>Github</a>
           </motion.div>
         </div>
+
+        {/* Project 2 */}
         <div className="flex flex-wrap lg:justify-center py-8 text-white">
           <motion.div
             whileInView={{ opacity: 1, x: 0 }}
             initial={{ opacity: 0, x: -100 }}
             transition={{ duration: 1 }}
             className='w-full lg:w-1/4'>
-            <img src='/assets/blog.jpeg' alt="OD request app" width={250} height={250} className='mb-6 rounded' />
-
-
+            <img src='/assets/blog.jpeg' alt="Blog Page" width={250} height={250} className='mb-6 rounded' />
           </motion.div>
           <motion.div 
-           whileInView={{opacity:1,x:0}}
-            initial={{opacity:0,x:100}}
-           transition={{duration:1}}
-          className='w-full max-w-xl flex flex-col lg:w-3/4'>
+           whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: 100 }}
+           transition={{ duration: 1 }}
+           className='w-full max-w-xl flex flex-col lg:w-3/4'>
             <h6 className='mb-2 font-extrabold'>Blog Page</h6>
-            <p className='mb-2 text-neutral-400'>This is a blog site where users can read and upload posts, Share their views, comment on posts. This site acts as an public forum where users can post anything, So this app also has a feature of checking the content for any sensitive or use of abusive language. </p>
-            <p className='text-sm pr-8'>React js,Django,SQL,Tailwindcss,Machine Learning</p>
-            <a  className="text-lg px-2 py-2 hover:animate-bounce font-extrabold" href='https://github.com/nadeemsheriff18/Blog_page'>Github</a>
+            <p className='mb-2 text-neutral-400 text-justify'>This is a blog site where users can read and upload posts, share their views, and comment on posts. This site acts as a public forum where users can post anything. The app also checks content for any sensitive or abusive language.</p>
+            <p className='text-sm'>Technologies: React, Django, SQL, Tailwind CSS, Machine Learning</p>
+            <a className="text-lg px-2 py-2 hover:animate-bounce font-extrabold" href='https://github.com/nadeemsheriff18/Blog_page'>Github</a>
           </motion.div>
         </div>
+
+        {/* Project 3 */}
         <div className="flex flex-wrap lg:justify-center py-8 text-white">
           <motion.div
             whileInView={{ opacity: 1, x: 0 }}
             initial={{ opacity: 0, x: -100 }}
             transition={{ duration: 1 }}
             className='w-full lg:w-1/4'>
-            <img src='/assets/ideaforge.png' alt="OD request app" width={250} height={250} className='mb-6 rounded' />
-
-
+            <img src='/assets/ideaforge.png' alt="Ideaforge Hackathon Website" width={250} height={250} className='mb-6 rounded' />
           </motion.div>
           <motion.div 
-           whileInView={{opacity:1,x:0}}
-            initial={{opacity:0,x:100}}
-           transition={{duration:1}}
-          className='w-full max-w-xl flex flex-col lg:w-3/4'>
+           whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: 100 }}
+           transition={{ duration: 1 }}
+           className='w-full max-w-xl flex flex-col lg:w-3/4'>
             <h6 className='mb-2 font-extrabold'>Ideaforge Hackathon Website</h6>
-            <p className='mb-2 text-neutral-400'>A webiste developed for the hackathon conducted by the department of computer science and business systems in my college.</p>
-            <p className='text-sm'>React js,Framer motion,Tailwindcss</p>
-            <a  className="text-lg px-2 py-2 hover:animate-bounce" href='https://github.com/nadeemsheriff18/ideaforge-website'>Github</a>
+            <p className='mb-2 text-neutral-400 text-justify'>A website developed for the hackathon conducted by the department of computer science and business systems in my college.</p>
+            <p className='text-sm'>Technologies: React, Framer Motion, Tailwind CSS</p>
+            <a className="text-lg px-2 py-2 hover:animate-bounce font-extrabold" href='https://github.com/nadeemsheriff18/ideaforge-website'>Github</a>
             <a className="text-lg px-2 py-2 hover:animate-bounce font-extrabold" href='https://ideaforgehackathon.netlify.app/'>Website</a>
           </motion.div>
         </div>
+
+        {/* Project 4 */}
         <div className="mb-8 flex flex-wrap lg:justify-center text-white">
           <motion.div
             whileInView={{ opacity: 1, x: 0 }}
             initial={{ opacity: 0, x: -100 }}
             transition={{ duration: 1 }}
             className='w-full lg:w-1/4'>
-            <img src='/assets/chatapp.png' alt="Chatapp" width={200} height={200} className='mb-6 rounded' />
-
-
+            <img src='/assets/chatapp.png' alt="Chat Application" width={200} height={200} className='mb-6 rounded' />
           </motion.div>
           <motion.div
             whileInView={{ opacity: 1, x: 0 }}
             initial={{ opacity: 0, x: 100 }}
             transition={{ duration: 1 }}
             className='w-full max-w-xl lg:w-3/4 flex flex-col'>
-            <h6 className='mb-2 font-extrabold'>Chat application</h6>
-            <p className='mb-2 text-neutral-400 text-pretty'>A one-one chat application ensures privacy and lets users text each other!</p>
-            <p className='text-sm'>React js,Express js,Postgresql,Tailwindcss</p>
-            <a  className="text-lg px-2 py-2 hover:animate-bounce font-extrabold" href='https://github.com/nadeemsheriff18/React-chatApp'>Github</a>
+            <h6 className='mb-2 font-extrabold'>Chat Application</h6>
+            <p className='mb-2 text-neutral-400 text-justify'>A one-on-one chat application that ensures privacy and lets users text each other!</p>
+            <p className='text-sm'>Technologies: React, Express, PostgreSQL, Tailwind CSS</p>
+            <a className="text-lg px-2 py-2 hover:animate-bounce font-extrabold" href='https://github.com/nadeemsheriff18/React-chatApp'>Github</a>
           </motion.div>
         </div>
       </div>
@@ -100,4 +100,4 @@ const Projects = () => {
   )
 }
 
-export default Projects
+export default Projects;
